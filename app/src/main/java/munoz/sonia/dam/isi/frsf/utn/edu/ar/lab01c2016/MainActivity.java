58bbb1b8d1f07d76b2c546f93afbb4d1bfc47435
sonianto <sonia.tsuki@gmail.com>
2016-09-04 20:21:26 -0300

    Initial commit

---
+++ app/src/main/java/munoz/sonia/dam/isi/frsf/utn/edu/ar/lab01c2016/MainActivity.java
@@ -0,0 +1,19 @@
+package munoz.sonia.dam.isi.frsf.utn.edu.ar.lab01c2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void onclickboton(View view) {
+
+    }
+}
---
+++ app/src/test/java/munoz/sonia/dam/isi/frsf/utn/edu/ar/lab01c2016/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package munoz.sonia.dam.isi.frsf.utn.edu.ar.lab01c2016;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
